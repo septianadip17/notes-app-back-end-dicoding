d13c78743f59783453a5c7e317ff6cfbfa31ccba
Septian Adi Pratama <septianadip7998@gmail.com>
2025-11-23 20:48:46 +0700

    implement add note functionality with success and failure responses

--- src/notes.js
+++ src/notes.js
@@ -1,3 +1,23 @@
-const notes = [];
+/* eslint-disable no-unused-vars */
+const { nanoid } = require('nanoid');
+const notes = require('./notes');
 
+const addNoteHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
+
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    title,
+    tags,
+    body,
+    id,
+    createdAt,
+    updatedAt,
+  };
+
+  notes.push(newNote);
+};
 module.exports = notes;
