c91f05b74f3398ae31b285efad8895bec1691c39
Septian Adi Pratama <septianadip7998@gmail.com>
2025-11-23 21:15:58 +0700

    add getAllNotesHandler to retrieve all notes

--- src/handler.js
+++ src/handler.js
@@ -41,4 +41,12 @@ const addNoteHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addNoteHandler };
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+
+module.exports = { addNoteHandler, getAllNotesHandler };
+
